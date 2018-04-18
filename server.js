91f0308491adcc94d0ef27af356eb66b35adaedf
Kuan-Ting Chou <gary9716@gmail.com>
2018-04-18 16:54:29 +0900

    add google map web component dependency and delegate api routing to apiRouter

--- server.js
+++ server.js
@@ -1,22 +1,36 @@
+//modules
+require('dotenv').config();
 const prpl = require('prpl-server');
-// Node.js notation for importing packages
 const express = require('express');
-const polymerBuildPath = './polymerApp/build';
+const apiRouter = require(__dirname + '/api-router');
 
-// Spin up a server
+//constants
+const port = process.env.PORT? process.env.PORT:3000;
+const appMode = process.env.APP_MODE? process.env.APP_MODE : 'development'; 
+const polymerBuildPath = __dirname + '/polymerApp/build';
+const polymerAppRootPath = __dirname + '/polymerApp';
+const polymerConfig = require(polymerBuildPath + '/polymer.json');
+
+//express server setup 
 var app = express();
-var polymerConfig = require(polymerBuildPath + '/polymer.json');
+app.use('/api', apiRouter); //let apiRouter handle api routing
 
-//if you're using differential serving, don't forget to set autoBasePath to true in polymer.json located at root directory of polymer app.
-app.use(express.static(polymerBuildPath));
+if(appMode === 'production') {
+    //if you're using differential serving, don't forget to set autoBasePath to true in polymer.json located at root directory of polymer app.
+    app.use(express.static(polymerBuildPath));
 
-//api routes
-app.get('/api/launch', (req, res, next) => res.send('boom'));
+    //polymer route
+    app.get('/*', prpl.makeHandler(polymerBuildPath, polymerConfig));
+}
+else {
+    app.use(express.static(polymerAppRootPath));
+    
+    //polymer route
+    app.get('/*', prpl.makeHandler(polymerAppRootPath, {
+        entrypoint: "index.html"
+    }));
+}
 
-//polymer route
-app.get('/*', prpl.makeHandler(polymerBuildPath, polymerConfig));
-
-// Tell the app to listen for requests on port 3000
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(port, function () {
+  console.log('server listening on port ' + port + ' in ' + appMode + ' mode');
 });
