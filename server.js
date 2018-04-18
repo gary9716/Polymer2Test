545cdf0306058226320673bee7af3d6a7d85861b
Kuan-Ting Chou <gary9716@gmail.com>
2018-04-18 14:45:42 +0900

    add prpl server for using differential serving of polymer app

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
+const prpl = require('prpl-server');
 // Node.js notation for importing packages
-var express = require('express');
+const express = require('express');
+const polymerBuildPath = './polymerApp/build';
 
 // Spin up a server
 var app = express();
+var polymerConfig = require(polymerBuildPath + '/polymer.json');
 
-// Serve static files from the main build directory
-app.use(express.static(__dirname + '/polymerApp/build/es5-bundled'));
+//if you're using differential serving, don't forget to set autoBasePath to true in polymer.json located at root directory of polymer app.
+app.use(express.static(polymerBuildPath));
 
-// Render index.html on the main page, specify the root
-app.get('/', function(req, res){
-  res.sendFile("index.html", {root: '.'});
-});
+//api routes
+app.get('/api/launch', (req, res, next) => res.send('boom'));
+
+//polymer route
+app.get('/*', prpl.makeHandler(polymerBuildPath, polymerConfig));
 
 // Tell the app to listen for requests on port 3000
 app.listen(3000, function () {
